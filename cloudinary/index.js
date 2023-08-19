e5a73f8a9b70f35c09799ed8e864ec3fde896d41
Michael Dawson <krayziejama@gmail.com>
2023-08-19 22:15:22 +0100

    using multer and cloudinary for user images and improving on styling. deleting obsolete code

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,7 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-// const express = require('express');
-// const multer = require('multer');
+
 
 
 cloudinary.config({
--- middleware.js
+++ middleware.js
@@ -1,38 +1,29 @@
 const { request } = require('express');
-const Request = require("./models/request");
-const Comment = require("./models/comment");
-const Reply = require('./models/reply')
-const Roadmap = require("./models/roadmap")
+// const Request = require("./models/request");
+// const Comment = require("./models/comment");
+// const Reply = require('./models/reply')
+// const Roadmap = require("./models/roadmap")
 const ExpressError = require("./utils/ExpressError");
 
-module.exports.isLoggedIn = (req, res, next) => {
-  const { id } = req.params;
-  if (!req.isAuthenticated()) {
-    req.session.returnTo = (req.query._method === 'DELETE' ? `/feedback/${id}` : req.originalUrl);
-    // req.flash("error", "You must be signed in first!");
-    return res.redirect("/login");
-  }
-  next();
-};
-
-// module.exports.validateRequest = (req, res, next) => {
-//     const { error } = requestSchema.validate(req.body)
-//     if(error){
-//         //map over error.details and turn into a string
-//         const msg  = error.details.map(el => el.message).join(', ')
-//         throw new ExpressError(msg, 400)
-//         //result.error.details is an array
-//     } else {
-//         next();
+// module.exports.isLoggedIn = (req, res, next) =>{
+//     // console.log("REQ.USER...", req.user);
+//     if(req.isAuthenticated()){
+//         return next();
 //     }
+//     res.redirect("/feedback/login");
 // }
 
-module.exports.isCommentAuthor = async(req, res, next) =>{
-    const { id, commentId } = req.params
-    const comment = await Comment.findById(commentId);
-    if(!comment.user.equals(req.user.id)) {
-         alert("You are not authorised");
-        return res.redirect(`/feedback/${id}`)
+// module.exports.isLoggedIn = (req, res, next) =>{
+//     // console.log("REQ.USER...", req.user);
+//     if(req.isAuthenticated()){
+//         return next();
+//     }
+//     res.redirect("/feedback/login");
+// }
+function isLoggedIn(req, res, next){
+    // console.log("REQ.USER...", req.user);
+    if(req.isAuthenticated()){
+        return next();
     }
-    next()
+    res.redirect("/feedback/login");
 }
