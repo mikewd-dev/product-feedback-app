4b44489bbd6a8fe0b135d433130df9d7ad408b1b
Michael Dawson <145623651+mikewd-dev@users.noreply.github.com>
2026-02-03 00:21:41 +0000

    updated main file  and requests.js

--- app.js
+++ app.js
@@ -87,9 +87,9 @@ function isLoggedIn(req, res, next){
 
 // app.use('/.netlify/functions/product-feedback-app-figma/app', router);
 
-app.all('*', (req, res, next)=>{
-    next(new ExpressError('Page Not Found', 404))
- })
+// app.all('*', (req, res, next)=>{
+//     next(new ExpressError('Page Not Found', 404))
+//  })
 
 // app.use((err, req, res, next)=>{
 //     const { statusCode = 500 } = err;
